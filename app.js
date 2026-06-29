b42315b5da7d4ac1448c82bad0a8871662b8604b
santoshml-lab <santoshkrsbg36@gmail.com>
2026-06-29 23:23:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 alert("JS START");
 
 const API_BASE = "https://neetlession.onrender.com/ai";
-//const SUPABASE_URL = "https://jpwjsaknniejzpokmire.supabase.co";
-//const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impwd2pzYWtubmllanpwb2ttaXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MzA2NjgsImV4cCI6MjA5ODMwNjY2OH0.0ur8EhnBmwZGNvBW_ql5pVzOy6OQ9pdDpds_BtYivMg";
+const SUPABASE_URL = "https://jpwjsaknniejzpokmire.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impwd2pzYWtubmllanpwb2ttaXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MzA2NjgsImV4cCI6MjA5ODMwNjY2OH0.0ur8EhnBmwZGNvBW_ql5pVzOy6OQ9pdDpds_BtYivMg";
 
 const supabase = window.supabase.createClient(
     SUPABASE_URL,
