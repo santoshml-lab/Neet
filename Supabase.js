413343452f7e1017ab15a739b7749bebe65e35ce
santoshml-lab <santoshkrsbg36@gmail.com>
2026-06-30 01:10:57 +0530

    Delete Supabase.js

--- Supabase.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_KEY = "YOUR_ANON_KEY";
-
-const supabase = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
-);
-
-console.log("✅ Supabase Connected");
