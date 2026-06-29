9c257bf39f6b7cb499488a81cd460d92b2815962
santoshml-lab <santoshkrsbg36@gmail.com>
2026-06-29 19:54:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 alert("JS START");
 
 const API_BASE = "https://neetlession.onrender.com/ai";
+const SUPABASE_URL = "https://jpwjsaknniejzpokmire.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impwd2pzYWtubmllanpwb2ttaXJlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI3MzA2NjgsImV4cCI6MjA5ODMwNjY2OH0.0ur8EhnBmwZGNvBW_ql5pVzOy6OQ9pdDpds_BtYivMg";
+
+const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
 
 // Flashcard Global Variables
 let flashcards = [];
