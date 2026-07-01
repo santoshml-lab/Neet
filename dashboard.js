270c48c95376afbf90f7060a11a4e8e37cc80e79
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-01 15:46:51 +0530

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -10,16 +10,5 @@ async function loadUser() {
     alert("Welcome " + user.email);
     
 }
-const { data, error } = await db
-    .from("users")
-    .select("*")
-    .eq("email", user.email)
-    .single();
-
-if (error) {
-    alert(error.message);
-} else {
-    console.log(data);
-    alert("XP: " + data.xp);
-}
+            
 loadUser();
