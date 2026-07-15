930b8568d93484da2871816d293d5443e5b2e9c5
santoshml-lab <santoshkrsbg36@gmail.com>
2026-07-16 01:13:58 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ async function checkLogin() {
     const { data: { session } } = await db.auth.getSession();
 
     if (!session) {
-        window.location.href = "Login.html";
+        window.location.href = "login.html";
     }
 
 }
